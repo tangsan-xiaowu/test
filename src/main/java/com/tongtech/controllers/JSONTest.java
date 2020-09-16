365c494fea25e052dc8cab9bc3a2d1d93fd4d40a
tangsan-xiaowu <51995296+tangsan-xiaowu@users.noreply.github.com>
2020-09-16 22:39:06 +0800

    Update JSONTest.java
    update

--- src/main/java/com/tongtech/controllers/JSONTest.java
+++ src/main/java/com/tongtech/controllers/JSONTest.java
@@ -19,6 +19,8 @@ public class JSONTest {
     private static  String filePath2 ;
 
     private static  String filePath3 ;
+    
+    private static  String filePath4 ;
 
     static {
         try {
