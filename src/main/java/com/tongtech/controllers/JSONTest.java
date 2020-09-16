ae7d20ac9bee171c168808f6876edf18f38256f6
feiyu <feiyu@yun.com>
2020-09-16 22:27:44 +0800

    update2

--- src/main/java/com/tongtech/controllers/JSONTest.java
+++ src/main/java/com/tongtech/controllers/JSONTest.java
@@ -14,7 +14,7 @@ import java.util.Properties;
 public class JSONTest {
     private static  String filePath ;
 
-    private static  String filePath1 ;//更新2
+    private static  String filePath1 ;//更新3
 
     static {
         try {
